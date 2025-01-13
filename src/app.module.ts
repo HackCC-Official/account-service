4e45eca05857f8302f7932604f3f155a38fb0314
jellyfishsh <hikurage@protonmail.com>
2025-01-13 11:25:01 -0800

    setup accounts, applications, and attendance boilerplate code for implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountsController } from './accounts/accounts.controller';
+import { ApplicationsController } from './applications/applications.controller';
+import { AttendanceController } from './attendance/attendance.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AccountsController, ApplicationsController, AttendanceController],
   providers: [AppService],
 })
 export class AppModule {}
