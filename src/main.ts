e33b3f8cdbcfd243aed8949eebf75e7ce62cdecf
chheangg <balldremz@gmail.com>
2025-01-29 23:01:41 -0800

    add swagger-ui to document endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true
   });
-  
+
+  app.setGlobalPrefix(process.env.NODE_ENV === 'production' ? 'account-service' : '')
+
+  const config = new DocumentBuilder()
+    .setTitle('Account Service')
+    .setDescription('Account Service API Documentation')
+    .setVersion('0.1')
+    .addTag('Account', 'Endpoints for Account API')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config, { ignoreGlobalPrefix: false });
+
+  console.log(process.env.NODE_ENV)
+
+  SwaggerModule.setup(
+    process.env.NODE_ENV === 'production' ? 'account-service/docs' : 'docs', 
+    app, 
+    documentFactory
+  );
+
   await app.listen(3000);
 }
 bootstrap();
