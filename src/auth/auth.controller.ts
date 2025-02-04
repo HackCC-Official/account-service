f90ccc67aa1e0629d031b23b5792e6ad55a4d9fb
Jellyfishsh <hikurage@protonmail.com>
2025-02-04 04:49:48 -0800

    Created signin endpoint, created signin logic, made note for bcrypt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RequestSignInDTO } from './request-auth.dto';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private authService: AuthService) {}
 
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    signIn(@Body() signInDto: RequestSignInDTO){
+        return this.authService.signIn(signInDto.email, signInDto.password)
+    }
+
+
+    
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AccountService } from 'src/account/account.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private accountsService: AccountService,
+        private jwtService: JwtService
+    ){}
+
+    async signIn(email: string, pass: string): Promise<{ access_token: string }> {
+        const account = await this.accountsService.getByEmail(email);
+        //TODO: PLEASE SET UP BCRYPT
+        if (account?.password !== pass) {
+            throw new UnauthorizedException();
+        }
+
+        const payload = {sub: account.id, email: account.email}
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        }
+
+
+    }
+}
