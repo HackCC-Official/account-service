6f905fa8eb7d7b61a8137be1dac385d51a8a801a
chheangg <balldremz@gmail.com>
2025-03-12 21:57:29 -0700

    fix error with account-module imports

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -6,10 +6,10 @@ import { AccountController } from './account.controller';
 import { AccountProducerModule } from 'src/account-producer/account-producer.module';
 import { Team } from 'src/team/team.entity';
 import { AuthModule } from 'src/auth/auth.module';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Account, Team]), AccountProducerModule, AuthModule, ConfigService],
+    imports: [TypeOrmModule.forFeature([Account, Team]), AccountProducerModule, AuthModule, ConfigModule],
     providers: [AccountService],
     controllers: [AccountController],
     exports: [AccountService]
